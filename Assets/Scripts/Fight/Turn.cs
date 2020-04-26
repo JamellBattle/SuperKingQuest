cdcaacf3af58b6951c7f8b676f6ab6398ad8f293
griffsking <griffking0325@hotmail.com>
2020-04-25 20:29:50 -0400

    Last Major Update
    Major Changes/Additions:
    * Reset button added to Game Over screen, resets the current fight scene
    * Turn counter added to the top left of the screen, displays the current turn
    * Certain moves now require some HP to perform
    * NextScene object has a name input for loading the next scene with that name
    * BIG bug fixes:
             * UI boxes now always move to their proper positions
             * Character sprites now always return to their proper positions smoothly at the end of each turn
             * Overlay border added, now UI elements that should be offscreen are always offscreen
             * Button hitboxes now act how they should; no longer able to click on one button only for the next one down to activate
    
    Minor Changes/Additions:
    * Player now flashes red when being hurt by burn and purple when being hurt by poison

--- Assets/Scripts/Fight/Turn.cs
+++ Assets/Scripts/Fight/Turn.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Turn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text turn;
+    int turnNum = 1;
 
-    // Update is called once per frame
-    void Update()
+    public void nextTurn()
     {
-        
+        turnNum++;
+        turn.text = turnNum.ToString();
     }
 }
--- Assets/Scripts/Fight/move.cs
+++ Assets/Scripts/Fight/move.cs
@@ -8,10 +8,19 @@ public class move : MonoBehaviour
 {
     public string moveName = "";
     public int damage = 0;
+    public int cost = 0;
     public TextMeshProUGUI displayName;
+    public TextMeshProUGUI displayCost;
 
     void Start()
     {
         displayName.text = moveName;
+        displayCost.text = "(" + cost.ToString() + " HP)";
+    }
+
+    void Update()
+    {
+        displayName.text = moveName;
+        displayCost.text = "(" + cost.ToString() + " HP)";
     }
 }
